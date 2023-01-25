86d62bc39c8e429e6be2ef3533a4b72ee390fa8c
RizwanBitbucket <rizuece786@gmail.com>
2023-01-25 23:23:10 +0400

    Added Bookeeypay sdk xcframework

---
+++ BookeeyPaySDK.xcframework/ios-arm64/BookeeyPaySDK.framework/Headers/BookeeyPaySDK.h
@@ -0,0 +1,19 @@
+//
+//  BookeeyPaySDK.h
+//  BookeeyPaySDK
+//
+//  Created by Mohammed Rizwan on 12/06/20.
+//  Copyright © 2020 Xenon4Pay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BookeeyPaySDK.
+FOUNDATION_EXPORT double BookeeyPaySDKVersionNumber;
+
+//! Project version string for BookeeyPaySDK.
+FOUNDATION_EXPORT const unsigned char BookeeyPaySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BookeeyPaySDK/PublicHeader.h>
+
+
